6d7ee93b6de1ec3cc5b02cc22ec6153f8cd878da
kagwathi <91314834+kagwathi@users.noreply.github.com>
2023-11-17 20:08:03 -1200

    Add files via upload

---
+++ src/cat2_oop/Cat2_oop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cat2_oop;
+
+/**
+ *
+ * @author 16129
+ */
+public class Cat2_oop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
